0574beb6262b5d1719a3c8356b48ea70878a2e0c
Christian Tapia <christian1020_94@hotmail.com>
2021-11-21 13:25:32 -0500

    test3+4
    test3 added and modified
    
    test4 added

--- test.js
+++ test.js
@@ -5,3 +5,10 @@ function test(param1, param2) {
 function test2(param1, param2) {
   console.log("abc2");
 }
+
+function test3(param1, param2) {
+  console.log("abc3");
+}
+function test4(param1, param2) {
+  console.log("abc");
+}
